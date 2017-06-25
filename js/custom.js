6e6c594d3f50d7ceee7985717eab3badb4bcf147
nalcalag <nalcalag@gmail.com>
2017-06-25 01:22:27 +0100

    Responsive Menu

--- js/custom.js
+++ js/custom.js
@@ -31,3 +31,28 @@ $(document).ready(function(){
 				$('html, body').animate({scrollTop: 0}, 300);
 			})
 		});
+
+// Menu Button
+$(document).ready(main);
+ 
+var contador = 1;
+ 
+function main(){
+	$('.menu_bar').click(function(){
+		$('.nav-menu').toggle(); 
+ 
+		if(contador == 1){
+			$('.nav-menu').animate({
+				left: '0'
+			});
+			contador = 0;
+		} else {
+			contador = 1;
+			$('nav-menu').animate({
+				left: '-100%'
+			});
+		}
+ 
+	});
+ 
+};
